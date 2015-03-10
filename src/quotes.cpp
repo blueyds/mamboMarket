1062078480b68cd7b4ba0a7156d14fd6b982e401
Craig Nunemaker <craig@nunemakerllc.com>
2015-03-10 03:09:45 +0000

    quotes.cpp edited online with Bitbucket

--- src/quotes.cpp
+++ src/quotes.cpp
@@ -24,6 +24,70 @@
 #include "stock.h"
 #include "stocks.h"
 #include "csv.h"
+#include <boost/asio/ip/tcp.hpp>
+
+void loadASIO()
+{
+	boost::asio::ip::tcp::iostream s;
+	std::string host;
+	host << "www.google.com";
+	std::string path;
+	path << "/";
+
+    // The entire sequence of I/O operations must complete within 60 seconds.
+    // If an expiry occurs, the socket is automatically closed and the stream
+    // becomes bad.
+    s.expires_from_now(boost::posix_time::seconds(60));
+
+    // Establish a connection to the server.
+    s.connect(host "http");
+    if (!s)
+    {
+      std::cout << "Unable to connect: " << s.error().message() << "\n";
+      return 1;
+    }
+
+    // Send the request. We specify the "Connection: close" header so that the
+    // server will close the socket after transmitting the response. This will
+    // allow us to treat all data up until the EOF as the content.
+    s << "GET " << path << " HTTP/1.0\r\n";
+    s << "Host: " << host << "\r\n";
+    s << "Accept: */*\r\n";
+    s << "Connection: close\r\n\r\n";
+
+    // By default, the stream is tied with itself. This means that the stream
+    // automatically flush the buffered output before attempting a read. It is
+    // not necessary not explicitly flush the stream at this point.
+
+    // Check that response is OK.
+    std::string http_version;
+    s >> http_version;
+    unsigned int status_code;
+    s >> status_code;
+    std::string status_message;
+    std::getline(s, status_message);
+    if (!s || http_version.substr(0, 5) != "HTTP/")
+    {
+      std::cout << "Invalid response\n";
+      return 1;
+    }
+    if (status_code != 200)
+    {
+      std::cout << "Response returned with status code " << status_code << "\n";
+      return 1;
+    }
+
+    // Process the response headers, which are terminated by a blank line.
+    std::string header;
+    while (std::getline(s, header) && header != "\r")
+      std::cout << header << "\n";
+    std::cout << "\n";
+
+    // Write the remaining data to output.
+    std::cout << s.rdbuf();
+  }
+	
+}
 
 void quotes::update(int periods)
 {
