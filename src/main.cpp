b4247943f2d4a23005c6b30ade1b40dbfaf4a326
Craig Nunemaker <craig@nunemakerllc.com>
2015-04-03 23:49:53 +0000

    main.cpp edited online with Bitbucket

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,12 @@
 
 int main (int argc, char *argv[])
 {
-	if (argc != 2) // we want a second argument
-		std::cout << "USage : "<< argv[0] << " <file_name>";
+	if (argc != 4) // we want a more argument
+		std::cout << "Usage : "<< argv[0] << " <stock_symbol> <postgres_host> <postgres_db>";
 	else
 	{
+		std::cout << "What is your password for postgres? :>";
+		 
 		std::cout << "Preparing to pull from internet still in main.cpp\n";
 		std::cout << argv[1]<<"\n";
 		std::cout << "calling the stock class now\n";
