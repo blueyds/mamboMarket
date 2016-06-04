2932d2bcfe9c50c66ff13b7f99a4b68cd50551b3
Craig Nunemaker <craig@nunemakercpa.com>
2016-06-04 10:33:06 -0500

    Fixing segmentation fault
    Changed SMA window list to a vector and added an internal counter for
    fast access to size

--- src/functions.h
+++ src/functions.h
@@ -1,10 +1,11 @@
-#include <list> //std::list
+#include <vector> //std::vector
 #include <numeric> //std::accumulate
 template <class T>
 class SMA 
 {
-	std::list<T> window_values;
+	std::vector<T> window_values;
 	int window;
+	int curSize =0;
 public:
 	SMA(int win)
 	{
@@ -14,10 +15,16 @@ public:
 	T operator()(T value)
 	{
 		window_values.push_back(value);
+		curSize = curSize++;
 		T val = 0;
 		if (window_values.size() > window) 
 		{
-			window_values.pop_front();
+			for (int i=0; i<curSize;i++)
+			{
+				window_values[i]=window_values[i+1];
+			}
+			window_values.pop_back();
+			curSize=curSize--;
 			T new_value;
 			T sum_value;
 			sum_value = std::accumulate(window_values.begin(),window_values.end(),0);
