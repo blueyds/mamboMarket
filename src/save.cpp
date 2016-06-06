15169f73a7aba852431b95c417139239831a976e
Craig Nunemaker <craig@nunemakercpa.com>
2016-06-05 20:11:24 -0500

    implemented a csv save procedure
    this save is more robust than a serializable function. this will be used
    for potential machine learning sets in the future.

--- src/save.cpp
+++ src/save.cpp
@@ -17,12 +17,99 @@ Heading options should be separated by comma and include:
 */
 void stock::save(std::string fname, std::string headings)
 {
-	
-	std::cout << "endtering save\n";
+	bool hasDATE(false);
+	bool hasOPEN(false);
+	bool hasCLOSE(false);
+	bool hasHIGH(false);
+	bool hasLOW(false);
+	bool hasVOLUME(false);
+	bool hasADJ(false);
+	bool hasSMA10(false);
 	std::stringstream ss(headings);
 	std::istream_iterator<std::string> begin(ss);
 	std::istream_iterator<std::string> end;
 	std::vector<std::string> vstrings(begin, end);
-	std::cout <<vstrings[0] << "\t" <<vstrings[1]<<"\n";
-	//std::copy(vstrings.begin(), vstrings.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+	int count = vstrings.size();
+	int columns=0;
+	for (int i = 0; i < vstrings.size(); i++)
+	{
+		if (vstrings[i].compare("DATE"))
+			{hasDATE=true;columns=columns+1;};
+		if (vstrings[i].compare("OPEN"))
+			{hasOPEN=true;columns=columns+1;};
+		if (vstrings[i].compare("CLOSE"))
+			{hasCLOSE=true;columns=columns+1;};
+		if (vstrings[i].compare("HIGH"))
+			{hasHIGH=true;columns=columns+1;};
+		if (vstrings[i].compare("LOW"))
+			{hasLOW=true;columns=columns+1;};
+		if (vstrings[i].compare("VOLUME"))
+			{hasVOLUME=true;columns=columns+1;};
+		if (vstrings[i].compare("ADJ"))
+			{hasADJ=true;columns=columns+1;};
+		if (vstrings[i].compare("SMA10"))
+			{hasSMA10=true;columns=columns+1;};
+	}
+	std::size_t csvfound;
+	csvfound=fname.find(".csv");
+	if (csvfound==std::string::npos) //fname does not have csv suffix
+		{fname << ".csv"; }
+	std::ofstream ofs (fname, std::ofstream::out);
+	count = dates.size();
+	for (int i = 0; i < count; i++)
+	{
+		std::string temp;
+		int counter=0;
+		if (hasDATE)
+		{
+			counter = counter +1;
+			temp << dates[i];
+			if (counter<columns){temp<<",";}
+		}
+		if (hasOPEN)
+		{
+			counter = counter +1;
+			temp << opening_prices[i];
+			if (counter<columns){temp<<",";}
+		}
+		if (hasCLOSE)
+		{
+			counter = counter +1;
+			temp << closing_prices[i];
+			if (counter<columns){temp<<",";}
+		}
+		if (hasHIGH)
+		{
+			counter = counter +1;
+			temp << highs[i];
+			if (counter<columns){temp<<",";}
+		}
+		if (hasLOW)
+		{
+			counter = counter +1;
+			temp << lows[i];
+			if (counter<columns){temp<<",";}
+		}
+		if (hasVOLUME)
+		{
+			counter = counter +1;
+			temp << volumns[i];
+			if (counter<columns){temp<<",";}
+		}
+		if (hasADJ)
+		{
+			counter = counter +1;
+			temp << adj_closes[i];
+			if (counter<columns){temp<<",";}
+		}
+		if (hasSMA10)
+		{
+			counter = counter +1;
+			temp << SMAs10[i];
+			if (counter<columns){temp<<",";}
+		}
+		temp<<"\n"
+		ofs << temp;
+	}
+	ofs.close();
 }
