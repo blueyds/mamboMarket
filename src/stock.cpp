01032d71e6d2bc462acef7feba55eeac630b2cbd
Craig Nunemaker <craig@nunemakercpa.com>
2016-06-11 10:14:55 -0500

    removed functincpp

--- src/stock.cpp
+++ src/stock.cpp
@@ -38,11 +38,9 @@ stock::stock(std::string sname){
 
 void stock::updateTA()
 {
-	TA::SMA(10,adj_closes,SMAs10);
-	/*TA::functors::SMA<double> sma10(10);
+	TA::functors::SMA<double> sma10(10);
 	SMAs10.reserve(adj_closes.size());
 	std::transform(adj_closes.begin(),adj_closes.end(),SMAs10.begin(),sma10);
-	*/
 }
 
 stock::stock(std::string sname,std::string name,std::string descr)
