fe159150366552855c1334b402dcc0c2f5c68254
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-08-14 18:02:28 -0500

    log level

--- test/sec_test.cpp
+++ test/sec_test.cpp
@@ -8,7 +8,7 @@
 using namespace boost::unit_test;
 BOOST_AUTO_TEST_CASE(xml_test)
 {
-	unit_test_log.set_threshold_level(messages);
+	unit_test_log::instance().set_threshold_level(log_messages);
 	sec::info s("AAPL");
 	std::string cik;
 	std::string sic;
