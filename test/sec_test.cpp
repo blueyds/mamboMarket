7e1fec031519a514cb235615f4e7e7b27908b540
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-08-14 21:43:09 -0500

    removed boost test

--- test/sec_test.cpp
+++ test/sec_test.cpp
@@ -1,22 +1,16 @@
 //add source header includes here
+#define TESTING
 #include <string>
-#include <iostream>
+#include "error.hpp"
 #include "sec/sec_report.hpp"
 
-#define BOOST_TEST_MODULE XMLtest
-#include <boost/test/included/unit_test.hpp>
-#define TESTING
-using namespace boost::unit_test;
-BOOST_AUTO_TEST_CASE(xml_test)
+int main()
 {
-	unit_test_log.set_threshold_level(log_messages);
 	sec::info s("AAPL");
 	std::string cik;
 	std::string sic;
 	cik=s.getCIK();
 	sic=s.getSIC();
-	BOOST_CHECK_MESSAGE(sic=="3571",sic);
-	BOOST_CHECK_MESSAGE(cik=="0000320193",cik);
-	BOOST_TEST_MESSAGE("CIK: "<<cik);
-	BOOST_TEST_MESSAGE("SIC: "<<sic);
+	MY_TEST_MESSAGE("CIK: "<<cik);
+	MY_TEST_MESSAGE("SIC: "<<sic);
 }
