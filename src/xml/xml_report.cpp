eadeaa7f4aa0f3566ca728dd5473af422a7bd81f
Craig Nunemaker <blueyds@users.noreply.github.com>
2017-08-23 21:24:33 -0500

    debug

--- src/xml/xml_report.cpp
+++ src/xml/xml_report.cpp
@@ -59,6 +59,7 @@ int sec::xml_report::getChildCount(std::initializer_list<int> indices, std::init
 	for (int i : indices)
 	{
 		child_node=parent_node->first_node((*tagIt).c_str());
+		MY_TEST_MESSAGE("index/tag:    "+i+"/"+(*tagIt));
 		if (i>0)
 		{	for(count=1;count<=i;i++)
 			{child_node=parent_node->next_sibling((*tagIt).c_str());}
@@ -68,6 +69,7 @@ int sec::xml_report::getChildCount(std::initializer_list<int> indices, std::init
 	}
 	//logic there should be one less index than tags. otherwise you would not be asking for a count
 	child_node=parent_node->first_node((*tagIt).c_str());
+	MY_TEST_MESSAGE("final tag count:    "+(*tagIt));
 	if(!child_node){count=0;}
 	else
 	{
